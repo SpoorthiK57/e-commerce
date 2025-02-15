04e89f6331b2ffc1699a28fa54de259043969a77
Spoorthi.Kamidi <Spoorthi.Kamidi@ever.ag>
2025-02-15 11:52:25 -0500

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<<<<<<< HEAD
 import ShopContextProvider from './Context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,14 @@ root.render(
   <ShopContextProvider>
     <App />
   </ShopContextProvider>
+=======
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+>>>>>>> 58c2f42 (Initialize project using Create React App)
 );
 
 // If you want to start measuring performance in your app, pass a function
