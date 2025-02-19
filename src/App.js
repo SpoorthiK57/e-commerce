df4372a47e3dca266356f7dfb9f0524d85ea8c28
Spoorthi.Kamidi <Spoorthi.Kamidi@ever.ag>
2025-02-18 21:56:14 -0500

    Added new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Product from "./Pages/Product";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />}></Route>
+          <Route path="/mens" element={<ShopCategory category="men" />}></Route>
+          <Route
+            path="/womens"
+            element={<ShopCategory category="women" />}
+          ></Route>
+          <Route
+            path="/kids"
+            element={<ShopCategory category="kids" />}
+          ></Route>
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />}></Route>
+          </Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/login" element={<LoginSignup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
