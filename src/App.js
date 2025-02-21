af71ade95e19a7b955a2a7ad35486705b2846ee7
Spoorthi.Kamidi <Spoorthi.Kamidi@ever.ag>
2025-02-20 20:45:19 -0500

    Added components and login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import LoginSignup from "./Pages/LoginSignup";
 import Product from "./Pages/Product";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
+import Footer from "./Components/Footer/Footer";
+import mens_banner from "./Components/Assets/banner_mens.png"
+import womens_banner from "./Components/Assets/banner_women.png"
+import kids_banner from "./Components/Assets/banner_kids.png"
 
 function App() {
   return (
@@ -14,14 +18,14 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />}></Route>
-          <Route path="/mens" element={<ShopCategory category="men" />}></Route>
+          <Route path="/mens" element={<ShopCategory banner={mens_banner} category="men" />}></Route>
           <Route
             path="/womens"
-            element={<ShopCategory category="women" />}
+            element={<ShopCategory  banner={womens_banner} category="women" />}
           ></Route>
           <Route
             path="/kids"
-            element={<ShopCategory category="kids" />}
+            element={<ShopCategory banner={kids_banner} category="kid" />}
           ></Route>
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />}></Route>
@@ -29,6 +33,7 @@ function App() {
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/login" element={<LoginSignup />}></Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
