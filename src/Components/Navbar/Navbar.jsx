55be140f88b736fa2db52d173cab88c12c263335
Spoorthi.Kamidi <Spoorthi.Kamidi@ever.ag>
2025-02-25 10:57:41 -0500

    Added changes to cart component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,15 @@
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {Link} from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 
 const Navbar = () => {
 
     const [menu, setMenu] = useState("shop");  
+    const {getTotalCartItems} = useContext(ShopContext);
     return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -24,7 +26,7 @@ const Navbar = () => {
         <div className='nav-login-cart'>
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-            <div className='nav-cart-count'></div>
+            <div className='nav-cart-count'>{getTotalCartItems()}</div>
 
         </div>
     </div>
